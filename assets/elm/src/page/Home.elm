3089d406e55929d4bcba6c52583df2e6da642346
Zack Kayser <zkayser@gmail.com>
2017-10-17 21:40:07 -0400

    Polish off home page view more

--- assets/elm/src/page/Home.elm
+++ assets/elm/src/page/Home.elm
@@ -35,10 +35,10 @@ view session model =
     , div [ class "row landing"]
       [  div [ class "col s12 m12 l4 landing-item" ]
         [ div [ class "card teal darken-2 landing-item" ]
-          [ div [ class "card-content white-text" ]
-            [ span [ class "card-title" ]
+          [ div [ class "card-content white-text text-center" ]
+            [ span [ class "card-title center-align" ]
               [ text "Create your own poker room and invite friends"]
-            , p [] 
+            , p [ class "medium-text"] 
               [ text """Sign up to create your own private poker rooms. Invite friends
                       or send invitations to other PokerEx players. Once you make a
                       private poker room, your game and table state will be kept alive
@@ -49,10 +49,10 @@ view session model =
         ]
       , div [ class "col s12 m12 l4 landing-item" ]
         [ div [ class "card cyan darken-3 landing-item" ]
-          [ div [ class "card-content white-text"]
-            [ span [ class "card-title"]
+          [ div [ class "card-content white-text text-center"]
+            [ span [ class "card-title center-align"]
               [ text "What is PokerEx?" ]
-            , p [] 
+            , p [ class "pop-text" ] 
               [ text """PokerEx is an online poker environment
                       designed to deliver an engaging, real-time
                       experience."""
@@ -62,10 +62,10 @@ view session model =
         ]
       , div [ class "col s12 m12 l4 landing-item" ]
         [ div [ class "card indigo darken-4 landing-item" ]
-          [ div [ class "card-content white-text" ]
-            [ span [ class "card-title" ]
+          [ div [ class "card-content white-text text-center" ]
+            [ span [ class "card-title center-align" ]
               [ text "Join our public tables for a quick round"]
-            , p []
+            , p [ class "medium-text" ]
               [ text """Once you join us on PokerEx, you can sit down and play at
                       any one of our public rooms. Public rooms are a perfect fit
                       if you are looking to play a short round or two and get on
@@ -81,14 +81,14 @@ viewButton : Maybe Player -> Html msg
 viewButton player =
   case player of
     Just player ->
-      -- This goes to the login page for the time being because we don't have an account page yet.
-      a [ class "waves-effect waves-light btn-large white-text", Route.href Route.Login ]
+      -- This goes to the login page for the time being because we don't have an account page yet
+      a [ class "waves-effect waves-light btn-large white-text margin-bottom-small", Route.href Route.Login ]
         [ i [ class "material-icons left large-text" ]
           [ text "account_box" ]
         , text "Go to your account"
         ]
     _ ->
-      a [ class "waves-effect waves-light btn-large white-text", Route.href Route.Register ]
+      a [ class "waves-effect waves-light btn-large white-text margin-bottom-small", Route.href Route.Register ]
         [ i [ class "material-icons left large-text" ]
           [ text "add_circle" ]
         , text "Join PokerEx Now!"
--- assets/elm/src/views/Footer.elm
+++ assets/elm/src/views/Footer.elm
@@ -8,26 +8,23 @@ view =
   footer [ class "page-footer blue-grey darken-3 white-text bottom"] 
     [ div [ class "container" ]
       [ div [ class "row footer-row"] 
-        [ div [ class "col l6 s12" ] 
+        [ div [ class "col m6 l6 s12 center-align-small" ] 
           [ h5 [ class "white-text" ] [ text "POKEREX"]
           , p [ class "grey-text text-lighten-4"] 
-            [ text "PokerEx was designed and developed by Zack Kayser."
-            , br [] []
+            [ text "PokerEx was designed and developed by Zack Kayser. "
             , text "Connect with me on Twitter at @kayserzl or on "
             , a [ href "https://www.linkedin.com/in/zack-kayser-93b96b88" ] [ text "LinkedIn. "]
-            , br [] []
             , text "Feel free to check out the open-source code on "
             , a [ href "https://github.com/zkayser/pokerex_client" ] [ text "Github"]
-            , text "!"
-            , br [] []
+            , text "! "
             , text "You can also check out the server-side Elixir/Phoenix project "
             , a [ href "https://github.com/zkayser/poker_ex" ] [ text "here"]
             , text "."
             ]
           ]
-        , div [ class "col l4 offset-l2 s12" ]
-          [ h5 [ class "white-text"] [ text "Links" ]
-          , ul [ class "text-center" ] 
+        , div [ class "col m4 l4 offset-l2 s12" ]
+          [ h5 [ class "white-text center-align-small"] [ text "Links" ]
+          , ul [ class "text-center"] 
             [ li [ ] [ a [ href "https://github.com/zkayser/pokerex_client"] [ text "Github"] ]
             , li [ ] [ a [ href "https://github.com/zkayser/poker_ex"] [ text "Server Side Elixir Project"] ]
             , li [ ] [ a [ href "http://elm-lang.org/" ] [ text "Elm-lang home page" ] ]
