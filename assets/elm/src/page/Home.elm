237f5c00cddc7614e415c739d6c4263fff71418d
Zack Kayser <zkayser@gmail.com>
2017-10-17 20:05:19 -0400

    Add sign up button and polish home page views

--- assets/elm/src/page/Home.elm
+++ assets/elm/src/page/Home.elm
@@ -2,6 +2,7 @@ module Page.Home exposing (..)
 
 import Data.Session as Session exposing (Session)
 import Data.Player as Player exposing (Player, usernameToString)
+import Route as Route exposing (Route)
 import Html exposing (..)
 import Html.Attributes exposing (style, class)
 
@@ -27,7 +28,9 @@ view session model =
   main_ []
     [ div [ class "hero valign-wrapper" ]
         [ h1 [ class "welcome", style [ ("text-align", "center")] ] 
-          [ text (model.greeting ++ personalizedGreeting) ] 
+          [ text (model.greeting ++ personalizedGreeting) ]
+        , br [] []
+        , viewButton session.player 
         ] 
     , div [ class "row landing"]
       [  div [ class "col s12 m12 l4 landing-item" ]
@@ -61,7 +64,7 @@ view session model =
         [ div [ class "card indigo darken-4 landing-item" ]
           [ div [ class "card-content white-text" ]
             [ span [ class "card-title" ]
-              [ text "Play with other PokerEx players in our public rooms"]
+              [ text "Join our public tables for a quick round"]
             , p []
               [ text """Once you join us on PokerEx, you can sit down and play at
                       any one of our public rooms. Public rooms are a perfect fit
@@ -73,6 +76,24 @@ view session model =
         ]
       ]
     ]
+
+viewButton : Maybe Player -> Html msg
+viewButton player =
+  case player of
+    Just player ->
+      -- This goes to the login page for the time being because we don't have an account page yet.
+      a [ class "waves-effect waves-light btn-large white-text", Route.href Route.Login ]
+        [ i [ class "material-icons left large-text" ]
+          [ text "account_box" ]
+        , text "Go to your account"
+        ]
+    _ ->
+      a [ class "waves-effect waves-light btn-large white-text", Route.href Route.Register ]
+        [ i [ class "material-icons left large-text" ]
+          [ text "add_circle" ]
+        , text "Join PokerEx Now!"
+        ]
+
 
 -- UPDATE --
 type Msg
