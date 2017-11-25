abd9b71849f2db979ba8f7310609893033a6a4c5
Zack Kayser <zack.kayser@kroger.com>
2017-11-25 16:57:04 -0500

    Encode payloads from Bank / Add Chip widget

--- assets/elm/src/Views/Bank.elm
+++ assets/elm/src/Views/Bank.elm
@@ -1,25 +1,47 @@
 module Views.Bank exposing (view)
 
 import Html as Html exposing (..)
-import Html.Events as Events exposing (onClick)
+import Html.Events as Events exposing (onClick, onInput, onSubmit)
 import Html.Attributes as Attrs exposing (..)
 import Json.Encode as Encode exposing (Value)
-import Data.Player as Player exposing (Username)
+import Data.Player as Player exposing (Player, Username)
 
-view : model -> (String -> msg, String -> Value -> msg) -> Html msg
+type alias Model r =
+  { r |
+    addAmount : Int
+  , player : Player
+  }
+
+view : Model r -> (String -> msg, String -> Value -> msg) -> Html msg
 view model (setMsg, submitMsg) =
   div [ class "bank-container center-align" ]
     [ span [ class "modal-header red-text" ] [ text "Add chips?"] 
     , div [ class "bank-form-container" ] 
       [ div [ class "bank-form" ]
-        [ div [ class "input-field" ]
-          [ input 
-            [ placeholder "How many chips would you like to add?"
-            , type_ "number"
-            , class "validate"
+        [ Html.form [ onSubmit (submitMsg "action_add_chips" (encodePayload model)) ]  
+          [ div [ class "input-field" ]
+            [ input 
+              [ placeholder "How many chips would you like to add?"
+              , type_ "number"
+              , class "validate"
+              , onInput setMsg
+              ]
+              []
             ]
-            []
           ]
+          , button 
+            [ type_ "submit"
+            , onClick (submitMsg "action_add_chips" (encodePayload model))
+            , class "btn blue white-text"
+            ]
+            [ text "Add chips"]
         ]
       ]
     ]
+
+encodePayload : Model r -> Value
+encodePayload model =
+  Encode.object [
+    ("player", Player.encodeUsername model.player.username)
+  , ("amount", Encode.int model.addAmount) 
+  ]
