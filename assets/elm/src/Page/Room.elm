7d66ac1e3cd65199d9dca43daafda0e032503e5d
Zack Kayser <zkayser@gmail.com>
2017-12-04 17:14:29 -0500

    Fix subscription that broke earlier and remove code from Room module

--- assets/elm/src/Page/Room.elm
+++ assets/elm/src/Page/Room.elm
@@ -4,26 +4,18 @@ import Data.Player as Player exposing (Player)
 import Data.Session as Session exposing (Session)
 import Data.Room as Room exposing (Room)
 import Data.RoomPage as RoomPage exposing (RoomPage)
-import Data.Card as Card exposing (Card)
-import Data.WinningHand as WinningHand exposing (WinningHand)
 import Page.Room.SocketConfig as SocketConfig exposing (..)
 import Page.Room.UpdateHelpers as Updaters exposing (..)
 import Page.Room.Helpers as Helpers exposing (..)
 import Page.Room.ViewHelpers exposing (..)
 import Types.Room.ModalState as ModalState exposing (..)
 import Types.Room.Messages as Messages exposing (..)
-import Html exposing (..)
+import Html exposing (Html, div, text)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onSubmit, onInput)
-import Dict exposing (Dict)
-import Mouse
 import Time exposing (Time)
 import Widgets.PlayerToolbar as PlayerToolbar
-import Widgets.Modal as Modal
 import Views.Actions as Actions
-import Views.Bank as Bank
-import Views.Account as Account
-import Views.Chat as Chat exposing (Chat)
 import Phoenix
 
 -- Types
@@ -74,108 +66,6 @@ view session model =
     , viewMessages model
     ]
   
--- WIDGET CONFIGURATIONS --
-toolbarConfig : Model -> PlayerToolbar.Config Msg
-toolbarConfig model =
-  let
-    hasJoined =
-      List.member model.player.username (List.map .name model.roomModel.players)
-    (txt, msg) =
-      if hasJoined then ("Leave", LeaveRoom model.player) else ("Join", JoinRoom model.player)
-    isActive = getIsActive model
-  in
-  { joinLeaveMsg = msg 
-  , btnText = txt 
-  , actionPressedMsg = ActionPressed
-  , isActive = isActive
-  , bankPressedMsg = BankPressed
-  , accountPressedMsg = AccountPressed
-  , chatPressedMsg = ChatPressed
-  , mobileToolbarPressed = MobileToolbarPressed
-  , closeModalMsg = CloseModal
-  }
-
-joinModalConfig : Model -> Modal.Config Msg
-joinModalConfig model =
-  { classes = ["white"]
-  , contentHtml = [ joinView model ]
-  , styles = Nothing
-  }
-  
-raiseModalConfig : Model -> Modal.Config Msg
-raiseModalConfig model =
-  { classes = ["white"]
-  , contentHtml = [ Actions.raiseContent (actionsViewConfig model) ]
-  , styles = Nothing
-  }
-  
-actionsViewConfig : Model -> Actions.ActionsModel Msg
-actionsViewConfig model =
-  let
-    isActive = getIsActive model
-    chips = getChips model model.roomModel.chipRoll
-    paidInRound = getChips model model.roomModel.round
-  in
-  { isActive = isActive
-  , chips = chips
-  , paidInRound = paidInRound
-  , toCall = model.roomModel.toCall
-  , player = model.player.username
-  , actionMsg = ActionMsg
-  , openRaiseMsg = OpenRaisePressed
-  , closeModalMsg = Blur
-  , closeRaiseMsg = CloseRaiseModal
-  , increaseRaiseMsg = IncreaseRaise
-  , decreaseRaiseMsg = DecreaseRaise
-  , setRaiseMsg = SetRaise
-  , raiseAmount = model.raiseAmount
-  , raiseMax = chips
-  , raiseMin = 0
-  , raiseInterval = model.raiseInterval
-  }
-  
-actionsModalConfig : Model -> Modal.Config Msg
-actionsModalConfig model =
-  { classes = ["white"]
-  , contentHtml = [ Actions.view (actionsViewConfig model) ]
-  , styles = Nothing  
-  }
-
-bankModalConfig : Model -> Modal.Config Msg
-bankModalConfig model =
-  { classes = ["white"]
-  , contentHtml = [ Bank.view model (SetAddAmount, ActionMsg, CloseModal) ]
-  , styles = Nothing
-  }
-
-accountModalConfig : Model -> Modal.Config Msg
-accountModalConfig model =
-  { classes = ["white"]
-  , contentHtml = [ Account.view model.player ]
-  , styles = Nothing
-  }
-
-chatModalConfig : Model -> Modal.Config Msg
-chatModalConfig model =
-  { classes = ["white"]
-  , contentHtml = [ Chat.view model.chat model.currentChatMsg SetChatMsg SubmitChat CloseModal ]
-  , styles = Just [ ("height", "40vh") ]
-  }
-
-mobileMenuConfig : Model -> Modal.Config Msg
-mobileMenuConfig model =
-  { classes = ["white"]
-  , contentHtml = [ PlayerToolbar.viewMobileMenu <| toolbarConfig model ]
-  , styles = Nothing
-  }
-  
-winningHandConfig : WinningHand -> Model -> Modal.Config Msg
-winningHandConfig winningHand model =
-  { classes = ["white", "winning-hand-modal"]
-  , contentHtml = [ viewWinningHandContent winningHand ] 
-  , styles = Nothing 
-  }
-
 -- UPDATE --
 update : Msg -> Model -> ( (Model, Cmd Msg), ExternalMsg )
 update msg model =
@@ -226,6 +116,10 @@ subscriptions model session =
   let
     phoenixSubscriptions =
       [ Phoenix.connect (socket session) model.channelSubscriptions ]
+    withBlur =
+      case model.modalRendered of
+        WinningHandModal _ -> Time.every 5000 ClearWinningHandModal
+        _ -> Sub.none
     withClearError =
       case model.errorMessages of
         [] -> Sub.none
@@ -235,4 +129,4 @@ subscriptions model session =
         [] -> Sub.none
         _ -> Time.every 3000 ClearRoomMessage
   in
-  Sub.batch (phoenixSubscriptions ++ [ withClearError, withClearRoomMessage ])
+  Sub.batch (phoenixSubscriptions ++ [ withBlur, withClearError, withClearRoomMessage ])
