7e44e991e1f905dfb7f6e343ccdf492a0e0a2812
Zack Kayser <zkayser@gmail.com>
2018-01-28 21:34:10 -0500

    More updates

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -50,7 +50,7 @@ exports.config = {
     },
     elmBrunch: {
       mainModules: ["elm/src/Main.elm"],
-      makeParameters: ['--output=main.js'],
+      makeParameters: ['--output=js/main.js'],
       outputFolder: "../assets/js"
     }
   },
