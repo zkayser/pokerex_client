286a1c29b78fe094a6b2719babcb15aac5fac38f
Zack Kayser <zkayser@gmail.com>
2018-01-31 22:56:03 -0500

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PokerExClient.Mixfile do
   def project do
     [
       app: :poker_ex_client,
-      version: "0.0.1",
+      version: "0.5.0",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
