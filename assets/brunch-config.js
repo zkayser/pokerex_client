ce85deb191270bf0391427babe6d48ee80665763
Kayser <zack.kayser@kroger.com>
2019-06-06 13:18:36 -0400

    Upgrade to Elm 0.19

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -50,7 +50,7 @@ exports.config = {
     },
     elmBrunch: {
       mainModules: ["elm/src/Main.elm"],
-      makeParameters: [""],
+      makeParameters: ["--debug"],
       outputFolder: "js/"
     },
   },
